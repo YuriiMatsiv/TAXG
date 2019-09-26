b9a80f97809efc5b924237f59a8baed4d53f3923
YuriiMatsiv <matsiv2017@ukr.net>
2019-09-26 11:56:23 +0300

    list fix

--- src/script.js
+++ src/script.js
@@ -11,15 +11,17 @@
 let navSlide = function () {
 
     const burger = document.querySelector(".burger");
-    const nav = document.querySelector('.menu');
+    const nav = document.querySelector('.header-menu');
     const navLinks = document.querySelectorAll('.navLinks li');
     const darkHeader = document.querySelector(".header");
+    const line = document.querySelector(".burger__line");
 
 
     burger.addEventListener('click', () => {
 
         nav.classList.toggle('nav-active');
         darkHeader.classList.toggle('dark-theme');
+        line.classList.toggle('burger__line_color');
 
         //slow motion
         // navLinks.forEach((link, index) => {
